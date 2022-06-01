81011c03e536d867fd218a6b8427c6eb842b6085
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-05-31 22:26:04 -0300

    namespaces

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,2 +1,16 @@
 "use strict";
+var area;
+(function (area) {
+    const PI = 3.14;
+    function circunferencia(raio) {
+        return PI * Math.pow(raio, 2);
+    }
+    area.circunferencia = circunferencia;
+    function retangulo(base, altura) {
+        return base * altura;
+    }
+    area.retangulo = retangulo;
+})(area || (area = {}));
+console.log(area.circunferencia(10));
+console.log(area.retangulo(10, 20));
 //# sourceMappingURL=namespaces.js.map
