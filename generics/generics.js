1100332bea7b7a0105c593e4ce7a0b2dab1d24b7
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-06-06 15:22:14 -0300

    Começando com Generics

---
+++ generics/generics.js
@@ -0,0 +1,6 @@
+"use strict";
+function echoMelhorado(objeto) {
+    return objeto;
+}
+console.log(echoMelhorado(27));
+//# sourceMappingURL=generics.js.map
---
+++ generics/generics.ts
@@ -0,0 +1,5 @@
+function echoMelhorado<T>(objeto: T): T {
+    return objeto
+}
+
+console.log(echoMelhorado(27))
