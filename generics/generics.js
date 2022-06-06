be61f2301ca57889abf5cf472746b4d71b7d82e6
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-06-06 16:24:59 -0300

    Terminando Genercis e iniciando Decorators

--- generics/generics.js
+++ generics/generics.js
@@ -1,6 +1,41 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const classes_1 = require("./../classes/classes");
 function echoMelhorado(objeto) {
     return objeto;
 }
 console.log(echoMelhorado(27));
+const avaliacoes = [10, 9.3, 7.7];
+avaliacoes.push(8.4);
+console.log(avaliacoes);
+// Class com Generics
+class OperacaoBinaria {
+    constructor(operando1, operando2) {
+        this.operando1 = operando1;
+        this.operando2 = operando2;
+    }
+}
+// console.log(new OperacaoBinaria(3, 7).executar())
+class SomaBinaria extends OperacaoBinaria {
+    executar() {
+        return this.operando1 + this.operando2;
+    }
+}
+console.log(new SomaBinaria(3, 8).executar());
+class DiferenacaEntreDatas extends OperacaoBinaria {
+    getTime(data) {
+        let { dia, mes, ano } = data;
+        return new Date(`${mes}/ ${dia}/${ano}`).getTime();
+    }
+    executar() {
+        const t1 = this.getTime(this.operando1);
+        const t2 = this.getTime(this.operando2);
+        const diferenca = Math.abs(t1 - t2);
+        const dia = 1000 * 60 * 60 * 24;
+        return `A difernça é de ${Math.ceil(diferenca / dia)} dia(s)`;
+    }
+}
+const novo = new classes_1.Data(1, 1, 2020);
+const d2 = new classes_1.Data(23, 2, 2020);
+console.log(new DiferenacaEntreDatas(novo, d2).executar());
 //# sourceMappingURL=generics.js.map
--- generics/generics.ts
+++ generics/generics.ts
@@ -1,5 +1,50 @@
+import { Data } from './../classes/classes';
 function echoMelhorado<T>(objeto: T): T {
     return objeto
 }
 
 console.log(echoMelhorado(27))
+
+const avaliacoes: Array <number> = [10, 9.3, 7.7]
+avaliacoes.push(8.4)
+console.log(avaliacoes)
+
+// Class com Generics
+
+abstract class OperacaoBinaria<T ,R> {
+    constructor(public operando1: T, public operando2: T) {
+
+    }
+
+    abstract executar() : R
+}
+
+// console.log(new OperacaoBinaria(3, 7).executar())
+
+class SomaBinaria extends OperacaoBinaria<number, number> {
+    executar(): number {
+        return this.operando1 + this.operando2
+    }
+}
+
+console.log(new SomaBinaria(3, 8).executar())
+
+class DiferenacaEntreDatas extends OperacaoBinaria<Data, string> {
+    getTime(data: Data): number {
+        let { dia, mes, ano } = data
+        return new Date(`${mes}/ ${dia}/${ano}`).getTime()
+    }
+
+    executar(): string {
+        const t1 = this.getTime(this.operando1)
+        const t2 = this.getTime(this.operando2)
+
+        const diferenca = Math.abs(t1 - t2)
+        const dia = 1000 * 60 * 60 * 24
+        return `A difernça é de ${Math.ceil(diferenca / dia)} dia(s)`
+    }
+} 
+
+const novo = new Data(1, 1, 2020)
+const d2 = new Data(23, 2, 2020)
+console.log(new DiferenacaEntreDatas(novo, d2).executar())
