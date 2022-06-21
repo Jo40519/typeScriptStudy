12643602ea2806f4bfabe15705bf2bb3a21ab410
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-06-20 21:56:28 -0300

    Update namespaces.ts

--- namespaces/namespaces.ts
+++ namespaces/namespaces.ts
@@ -109,3 +109,54 @@
 // }
 
 // console.log(solution([1, 2, 10, 50, 5]))
+
+// export function xo(str: string) {
+//     let arr = str.split('').map(elemento => {
+//         if (elemento == 'x') {
+//             return true
+//         } else if (elemento == 'o') {
+//             return true
+//         } else if (elemento != 'x' && elemento != 'o') {
+//             return false
+//         }
+//     })
+//     if (arr) {
+//             return true
+//     } else {
+//         return false
+//         }
+//     return arr
+// }
+
+// console.log(xo('xo'))
+
+// export function apple(x: any | string): string {
+
+//   if((x * x) >= 1000) {
+//     return 'It\'s hotter than the sun!!'
+//   } else {
+//     return 'Help yoursel to a honeycomb Yorkie for the glovebox.'
+//   }
+// }
+
+// console.log(apple('50'))
+
+// export function doubleChar(str: string): string{
+//     let arr = str.split('').map(elemento => {
+//         return elemento + elemento
+//     }).join('')
+//     console.log(arr)
+//     return arr
+// }
+
+// console.log(doubleChar('Adidas'))
+
+export 
+function predictAge(age1:number, age2:number, age3:number,age4:number,age5:number,age6:number,age7:number,age8:number){
+  let arr = [age1, age2, age3, age4, age5, age6, age7, age8].map(idade => {
+    return idade * idade
+  }).join('')
+    let ma = Math.pow(...arr, 2)
+};
+
+console.log(predictAge(65,60,75,55,60,63,64,45))
